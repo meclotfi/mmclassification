d8695446c01bb0b444495de2df19b98ac069bf44
Meclotfi <hl_mecharbat@esi.dz>
2022-05-22 23:06:50 +0200

    Create mobilevit.py

---
+++ configs/mobilenet_v2/mobilevit.py
@@ -0,0 +1,15 @@
+_base_ = [
+    '../_base_/datasets/cifar10_tl.py',
+    '../_base_/default_runtime.py'
+]
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(type='MobileViT'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=10,
+        in_channels=80,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1,),
+    ))
