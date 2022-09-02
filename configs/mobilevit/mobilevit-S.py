6bde7c30d35d13407522275fb2deaf7ce2cff075
Meclotfi <hl_mecharbat@esi.dz>
2022-09-02 17:23:21 +0200

    add mbvit

---
+++ configs/mobilevit/mobilevit-S.py
@@ -0,0 +1,75 @@
+_base_ = [
+    '../_base_/datasets/vww.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/vww.py'
+]
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(type='MobileViT',
+                    out_indices=(4,),
+                    Layers_config={
+                                                "layer1": {
+                                                        "type":"mobilenet2",
+                                                        "out_channels": 32,
+                                                        "expand_ratio": 2,
+                                                        "num_blocks": 1,
+                                                        "stride": 1,
+                                                        
+                                                    },
+                                                "layer2": {
+                                                        "type":"mobilenet2",
+                                                        "expand_ratio": 4,
+                                                        "out_channels": 64,
+                                                        "num_blocks": 3,
+                                                        "stride": 2,
+                                                        
+                                                    },
+                                                "layer3": {  # 28x28
+                                                        "type":"mobilevit",
+                                                        "out_channels": 96,
+                                                        "head_dim": 36,
+                                                        "ffn_dim": 288,
+                                                        "n_transformer_blocks": 2,
+                                                        "patch_h": 2,  # 8,
+                                                        "patch_w": 2,  # 8,
+                                                        "stride": 2,
+                                                        "mv_expand_ratio": 4,
+                                                        "num_heads": 4,
+                                                        
+                                                    },
+                                                "layer4": {  # 14x14
+                                                        "type":"mobilevit",
+                                                        "out_channels": 128,
+                                                        "head_dim": 43,
+                                                        "ffn_dim": 384,
+                                                        "n_transformer_blocks": 4,
+                                                        "patch_h": 2,  # 4,
+                                                        "patch_w": 2,  # 4,
+                                                        "stride": 2,
+                                                        "mv_expand_ratio": 4,
+                                                        "num_heads": 4,
+                                                        
+                                                    },
+                                                "layer5": {  # 7x7
+                                                        "type":"mobilevit",
+                                                        "out_channels": 160,
+                                                        "head_dim": 60,
+                                                        "ffn_dim": 480,
+                                                        "n_transformer_blocks": 3,
+                                                        "patch_h": 2,
+                                                        "patch_w": 2,
+                                                        "stride": 2,
+                                                        "mv_expand_ratio": 4,
+                                                        "num_heads": 1,
+                                                        
+                                                    },
+                                            },
+                    ),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=2,
+        in_channels=128,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1,)
+    ))
